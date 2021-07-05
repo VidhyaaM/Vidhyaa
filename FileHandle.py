4c1450defee9cab0f4c4ae1561d2feb0651d2e63
VidhyaaM <86224058+VidhyaaM@users.noreply.github.com>
2021-07-05 16:32:38 +0530

    Add files via upload

---
+++ FileHandle.py
@@ -0,0 +1,17 @@
+import os
+pathname= input("Enter the folder path: ")
+file_ext = input("Input the File extension: ")
+os.chdir(pathname)
+print(os.getcwd())
+print(os.listdir())
+
+def write_file(file_path):
+    with open("filenames.txt","w") as f:
+        f.write(file_path)
+  
+for file in os.listdir():
+    if file.endswith("."+file_ext):
+        file_path = f"{pathname}\{file}"
+        write_file(file_path)
+    
+    
